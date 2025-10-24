314324aaaeb41c5da621d7f98dfa0838f89e1bd1
KamilMatejuk <kamilmatejuk@gmail.com>
2025-10-24 21:09:58 +0200

    fix index error

--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ import pandas as pd
 
 def get_airport_city(code: str) -> str:
     df = pd.read_csv("airport-codes.csv")
-    city = df.loc[df['iata_code'] == code.upper(), 'municipality'].iloc[0]
-    country = df.loc[df['iata_code'] == code.upper(), 'iso_country'].iloc[0]
-    return f"{city}, {country}"
+    try:
+        city = df.loc[df['iata_code'] == code.upper(), 'municipality'].iloc[0]
+        country = df.loc[df['iata_code'] == code.upper(), 'iso_country'].iloc[0]
+        return f"{city}, {country}"
+    except IndexError:
+        return f"Unknown {code.upper()}"
