6b95e5f96612695b8d3663b68b480f8e8fb34023
KamilMatejuk <kamilmatejuk@gmail.com>
2026-01-09 14:28:01 +0100

    fix overriding last digits of Y

--- check_mouse_position.py
+++ check_mouse_position.py
@@ -6,7 +6,7 @@ print("Press Ctrl+C to stop\n")
 try:
     while True:
         x, y = pyautogui.position()
-        print(f"X={x}, Y={y}", end="\r")  # overwrite line
+        print(f"X={x}, Y={y}   ", end="\r")  # overwrite line
         time.sleep(0.1)
 except KeyboardInterrupt:
     print("\nStopped.")
